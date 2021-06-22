e7f4e9d30768fee20dc0658a650886703c537d44
Alexander Bedoya Espinel <alex22122020@gmail.com>
2021-06-22 17:09:38 -0500

    Adjunto los archivos del desafio numero 1 de javascript

---
+++ script.js
@@ -0,0 +1,9 @@
+const nombre = prompt("Ingrese su Nombre Por favor");
+alert("Su nombre es " + nombre);
+const apellido = prompt("Ingrese su Apellido por favor");
+alert("Su apellido es " + apellido);
+const edad = parseInt(prompt("Ingrese su Edad por favor"));
+alert("su edad es "+ edad);
+
+alert("Sea usted bienvenido Sr o Sra: " + nombre + ", Su estimado apellido es: " + apellido + " y Su edad es: "+ edad);
+alert("Esta muy joven ;)" + " , muchas gracias"); 
